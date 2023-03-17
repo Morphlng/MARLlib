8aec1053ef36b23ebb5b7bc431469f7bf7d4b57f
Morphlng <1193115470@qq.com>
2023-03-17 18:02:47 +0800

    adpat to latest MACarla-Gym

--- envs/base_env/macad.py
+++ envs/base_env/macad.py
@@ -28,6 +28,11 @@ policy_mapping_dict = {
 class RllibMacad(MultiAgentEnv):
     def __init__(self, env_config):
         map_name = env_config.get("map_name", "default")
+        self.use_only_semantic = env_config.get("use_only_semantic", False)
+        self.use_only_camera = env_config.get("use_only_camera", False)
+        assert not (
+            self.use_only_semantic and self.use_only_camera), "use_only_semantic and use_only_camera can not be True at the same time"
+
         env_class = env_name_mapping[map_name]
         if map_name != "custom":
             self.env = env_class()
@@ -36,30 +41,41 @@ class RllibMacad(MultiAgentEnv):
             self.env = env_class(env_config)
             self.env_config = env_config
 
-        self.num_agents = len(self.env_config["actors"].keys())
+        if self.use_only_semantic:
+            assert self.env_config["env"]["send_measurements"], "use_only_semantic can only be True when send_measurement is True"
+
+        self.num_agents = len(self.env_config["actors"])
         self.agents = ["car_{}".format(i) for i in range(self.num_agents)]
 
-        actor_id = next(iter(self.env_config["actors"].keys()))
-
-        # TODO: macad does not have global state yet
         # Note1: obs is the partial observation of the agent, state is the global state
-        # Note2: if enable send_measurements, the obs will be a Tuple of (obs, next_command, [speed, dist_to_goal])
-        # We would use only obs as the observation
+        # Note2: if enable send_measurements, the obs will be a Tuple of (obs, semantic_info)
+        actor_id = next(iter(self.env_config["actors"].keys()))
         obs_space = self.env.observation_space[actor_id]
-        if isinstance(obs_space, GymTuple) or isinstance(obs_space, tuple):
-            obs_space = obs_space[0]
+
+        if isinstance(obs_space, (GymTuple, tuple)):
+            image_space = obs_space[0]
             self.obs_with_measurement = True
+            if self.use_only_camera:
+                obs_space = image_space
+            elif self.use_only_semantic:
+                obs_space = obs_space[1]
         else:
             self.obs_with_measurement = False
+            image_space = obs_space
 
-        self.observation_space = GymDict({
+        obs_dict = {
             "obs": obs_space,
-            # state represent other agent's obs
-            "state": Box(
-                low=-np.inf,
-                high=np.inf,
-                shape=(obs_space.shape[0], obs_space.shape[1], obs_space.shape[2] * (self.num_agents - 1)))
-        })
+        }
+
+        if self.use_only_camera:
+            obs_dict.update({       # state represent other agent's obs
+                "state": Box(
+                    low=-np.inf,
+                    high=np.inf,
+                    shape=(image_space.shape[0], image_space.shape[1], image_space.shape[2] * (self.num_agents - 1)))
+            })
+
+        self.observation_space = GymDict(obs_dict)
 
         # Note: The action_space and observation_space here
         # represent single agent's action and observation space
@@ -70,40 +86,45 @@ class RllibMacad(MultiAgentEnv):
 
         obs = {}
         for actor_id in origin_obs.keys():
-            obs[actor_id] = {
-                "obs": origin_obs[actor_id][0] if self.obs_with_measurement else origin_obs[actor_id],
-                "state": np.concatenate([origin_obs[id][0] if self.obs_with_measurement else origin_obs[id]
-                                         for id in origin_obs.keys()
-                                         if id != actor_id], axis=-1)
-            }
+            if self.use_only_semantic:
+                obs[actor_id] = {
+                    "obs": origin_obs[actor_id][1],
+                }
+            elif self.use_only_camera:
+                obs[actor_id] = {
+                    "obs": origin_obs[actor_id][0] if self.obs_with_measurement else origin_obs[actor_id],
+                    "state": np.concatenate([origin_obs[id][0] if self.obs_with_measurement else origin_obs[id]
+                                             for id in origin_obs.keys()
+                                             if id != actor_id], axis=-1)
+                }
+            else:
+                obs[actor_id] = {
+                    "obs": origin_obs[actor_id]
+                }
 
         return obs
 
     def step(self, action_dict):
-        alive_actors = list(action_dict.keys())
-
-        for actor_id in self.env_config["actors"].keys():
-            if actor_id not in alive_actors:
-                action_dict[actor_id] = 4  # brake
 
         origin_obs, r, d, i = self.env.step(action_dict)
 
         obs = {}
         for actor_id in origin_obs.keys():
-            obs[actor_id] = {
-                "obs": origin_obs[actor_id][0] if self.obs_with_measurement else origin_obs[actor_id],
-                "state": np.concatenate([origin_obs[id][0] if self.obs_with_measurement else origin_obs[id]
-                                        for id in origin_obs.keys()
-                                        if id != actor_id], axis=-1)
-            }
-
-        # remove dead actor's observation
-        for actor_id in list(obs.keys()):
-            if actor_id not in alive_actors:
-                del obs[actor_id]
-                del r[actor_id]
-                del d[actor_id]
-                del i[actor_id]
+            if self.use_only_semantic:
+                obs[actor_id] = {
+                    "obs": origin_obs[actor_id][1],
+                }
+            elif self.use_only_camera:
+                obs[actor_id] = {
+                    "obs": origin_obs[actor_id][0] if self.obs_with_measurement else origin_obs[actor_id],
+                    "state": np.concatenate([origin_obs[id][0] if self.obs_with_measurement else origin_obs[id]
+                                             for id in origin_obs.keys()
+                                             if id != actor_id], axis=-1)
+                }
+            else:
+                obs[actor_id] = {
+                    "obs": origin_obs[actor_id]
+                }
 
         return obs, r, d, i
 
