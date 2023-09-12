b5228329a3eba2a61ba95bdc23319a72769a06c8
David Nie <1193115470@qq.com>
2023-09-12 17:29:52 +0800

    apply to latest dict-like observation

--- marllib/envs/base_env/macad.py
+++ marllib/envs/base_env/macad.py
@@ -1,14 +1,21 @@
-"""
-    This file is a wrapper for Macad-Gym. 
-"""
+'''
+Author: Morphlng
+Date: 2023-08-09 19:34:29
+LastEditTime: 2023-09-12 17:28:26
+LastEditors: Morphlng
+Description: Wrapper for macad env to restruct the observation and action space
+FilePath: \MARLlib\marllib\envs\base_env\macad.py
+'''
+
+import sys
 from copy import deepcopy
 
 import numpy as np
 from gym.spaces import Box
 from gym.spaces import Dict as GymDict
-from gym.spaces import Tuple as GymTuple
-from macad_gym.envs import (MultiCarlaEnv, Navigation, Strike, Town01Sim,
-                            Town03Sim, Town05Sim, Town11Sim, HomoNcomIndePOIntrxMASS3CTWN3)
+from macad_gym.envs import (HomoNcomIndePOIntrxMASS3CTWN3, MultiCarlaEnv,
+                            Navigation, Strike, Town01Sim, Town03Sim,
+                            Town05Sim, Town11Sim)
 from ray.rllib.env.multi_agent_env import MultiAgentEnv
 
 env_name_mapping = {
@@ -41,55 +48,50 @@ class RllibMacad(MultiAgentEnv):
         self.map_name = config.get("map_name", "default")
         self.use_only_semantic = config.get("use_only_semantic", False)
         self.use_only_camera = config.get("use_only_camera", False)
-        assert not (
-            self.use_only_semantic and self.use_only_camera), "use_only_semantic and use_only_camera can not be True at the same time"
+
+        if self.use_only_semantic and self.use_only_camera:
+            raise ValueError(
+                "`use_only_semantic` and `use_only_camera` can not be True at the same time")
 
         env_class = env_name_mapping[self.map_name]
-        if self.map_name != "custom":
-            self.env = env_class()
-        else:
-            self.env = env_class(config)
+        self.env: MultiCarlaEnv = env_class(config) if self.map_name == "custom" else env_class()
         self.env_config = self.env.configs
 
-        if self.use_only_semantic:
-            assert self.env_config["env"]["send_measurements"], "use_only_semantic can only be True when send_measurement is True"
+        if self.use_only_semantic and not self.env.env_obs.send_measurements:
+            raise ValueError(
+                "`use_only_semantic` cannot be True when `send_measurement` is False")
 
         self.agents = [actor_id for actor_id in self.env_config["actors"]
                        if actor_id not in self.env._ignore_actor_ids and actor_id != "ego"]
         self.num_agents = len(self.agents)
 
-        # Note1: obs is the partial observation of the agent, state is the global state
-        # Note2: if enable send_measurements, the obs will be a Tuple of (obs, semantic_info)
+        # Get observation space
         actor_id = next(iter(self.env_config["actors"].keys()))
         obs_space = self.env.observation_space[actor_id]
 
-        if isinstance(obs_space, (GymTuple, tuple)):
-            image_space = obs_space[0]
-            self.obs_with_measurement = True
-            if self.use_only_camera:
-                obs_space = image_space
-            elif self.use_only_semantic:
-                obs_space = obs_space[1]
-        else:
-            self.obs_with_measurement = False
-            image_space = obs_space
-
-        obs_dict = {
-            "obs": obs_space,
-        }
-
         if self.use_only_camera:
-            obs_dict.update({       # state represent other agent's obs
+            image_space = obs_space["camera"]
+            obs_dict = {
+                "obs": image_space,
                 "state": Box(
                     low=-np.inf,
                     high=np.inf,
                     shape=(image_space.shape[0], image_space.shape[1], image_space.shape[2] * (self.num_agents - 1)))
-            })
+            }
+        elif self.use_only_semantic:
+            obs_dict = {
+                "obs": obs_space["state"],
+            }
+        else:
+            obs_dict = {
+                "obs": obs_space,
+            }
+
+        if "action_mask" in obs_space.spaces:
+            env_config["mask_flag"] = True
+            obs_dict.update({"action_mask": obs_space["action_mask"]})
 
         self.observation_space = GymDict(obs_dict)
-
-        # Note: The action_space and observation_space here
-        # represent single agent's action and observation space
         self.action_space = self.env.action_space[actor_id]
 
     def _hard_reset(self):
@@ -98,10 +100,7 @@ class RllibMacad(MultiAgentEnv):
             self.env.close()
 
         env_class = env_name_mapping[self.map_name]
-        if env_class == MultiCarlaEnv:
-            self.env = MultiCarlaEnv(self.env_config)
-        else:
-            self.env = env_class()
+        self.env = MultiCarlaEnv(self.env_config) if env_class == MultiCarlaEnv else env_class()
         self.env_config = self.env.configs
 
     def reset(self):
@@ -111,7 +110,7 @@ class RllibMacad(MultiAgentEnv):
                 origin_obs = self.env.reset()
                 break
             except KeyboardInterrupt as e:
-                raise e
+                sys.exit(-1)
             except Exception as e:
                 print("Exception raised when reset: {}".format(e))
                 print("Reset failed, try hard reset")
@@ -121,16 +120,15 @@ class RllibMacad(MultiAgentEnv):
         return obs
 
     def step(self, action_dict):
-
         # We add this only to update the observation
         # This action will not take effect
         if "ego" in self.env_config["actors"]:
-            action_dict["ego"] = 0 if self.env_config["env"]["discrete_actions"] else (0, 0)
+            action_dict["ego"] = 0 if self.env.env_action.is_discrete() else (0, 0)
 
         try:
             origin_obs, r, d, i = self.env.step(action_dict)
         except KeyboardInterrupt as e:
-            raise e
+            sys.exit(-1)
         except Exception as e:
             print("Exception raised when step: {}".format(e))
             print("Step failed, set done to True and try hard reset on next reset")
@@ -149,20 +147,16 @@ class RllibMacad(MultiAgentEnv):
             if actor_id not in ["ego", "global"]:
                 if self.use_only_semantic:
                     obs[actor_id] = {
-                        "obs": o[actor_id][1],
+                        "obs": o[actor_id]["state"],
                     }
                 elif self.use_only_camera:
                     obs[actor_id] = {
-                        "obs": o[actor_id][0] if self.obs_with_measurement else o[actor_id],
-                        "state": np.concatenate([o[id][0] if self.obs_with_measurement else o[id]
-                                                for id in o.keys()
-                                                if id != actor_id], axis=-1)
+                        "obs": o[actor_id]["camera"],
+                        "state": np.concatenate([o[id]["camera"] for id in o.keys() if id != actor_id], axis=-1)
                     }
-                else:
-                    obs[actor_id] = {
-                        "obs": o[actor_id][1],
-                        "state": o[actor_id][0]
-                    }
+
+                if "action_mask" in o[actor_id]:
+                    obs[actor_id]["action_mask"] = o[actor_id]["action_mask"]
 
                 reward[actor_id] = r[actor_id] if r is not None else 0
                 done[actor_id] = d[actor_id] if d is not None else True
